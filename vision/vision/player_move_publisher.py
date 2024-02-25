b72d9551f821f24d537a689d3a1fd88aeb7acab6
MRHP1 <148250477+MRHP1@users.noreply.github.com>
2024-02-25 12:38:19 +0700

    Add files via upload

--- vision/vision/player_move_publisher.py
+++ vision/vision/player_move_publisher.py
@@ -4,37 +4,26 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 
-class VisionPublisher(Node):
+class WebcamPublisherNode(Node):
     def __init__(self):
-        super().__init__('vision_publisher')
-        self.publisher_ = self.create_publisher(Image, 'webcam', 10)
+        super().__init__('webcam_publisher_node')
+        self.publisher_ = self.create_publisher(Image, 'webcam_topic', 10)
+        self.timer = self.create_timer(1.0 / 30, self.publish_frame)  # Adjust frame rate as needed
         self.bridge = CvBridge()
-        self.timer = self.create_timer(1.0 / 30, self.publish_frame)  # Adjust the publishing rate as needed
-        self.cap = cv2.VideoCapture(0)
-        cv2.namedWindow('Webcam')  # Create a window for the webcam feed
+        self.cap = cv2.VideoCapture(0)  # Initialize webcam capture
 
     def publish_frame(self):
-        ret, frame = self.cap.read()
+        ret, frame = self.cap.read()  # Read frame from webcam
         if ret:
-            frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)  # Resize the frame to half
-            cv2.imshow('Webcam', frame)  # Display the frame in the 'Webcam' window
-
-            img_msg = self.bridge.cv2_to_imgmsg(frame, "bgr8")
-            self.publisher_.publish(img_msg)
-
-            # Check for 'q' key press to exit
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                self.destroy_node()
-                rclpy.shutdown()
-                self.cap.release()
-                cv2.destroyAllWindows()
-
+            msg = self.bridge.cv2_to_imgmsg(frame, encoding='bgr8')  # Convert frame to ROS Image message
+            self.publisher_.publish(msg)  # Publish frame
 
 def main(args=None):
     rclpy.init(args=args)
-    vision_publisher = VisionPublisher()
-    rclpy.spin(vision_publisher)
-    
+    webcam_publisher_node = WebcamPublisherNode()
+    rclpy.spin(webcam_publisher_node)
+    webcam_publisher_node.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
